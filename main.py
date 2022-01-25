d191a3568eb757ca1a69249748a8d0b438667dfa
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 19:50:36 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+friends = ["Peter", "Mark", "John", "Luke"]
+
+print(friends.index("Mark"))
+print(friends.count("Mark"))
+friends.sort()
+print(friends)
+friends.reverse()
+print(friends)
+friends2 = friends.copy()
+print(friends2)
